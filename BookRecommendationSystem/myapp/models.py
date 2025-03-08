abc800f21a7dad35dc1b58453439471be3a4d546
Adriana <adrianajaworska1717@gmail.com>
2025-03-08 21:03:29 +0100

    comments for database

--- BookRecommendationSystem/myapp/models.py
+++ BookRecommendationSystem/myapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+
+""" when Library is name of the database
+class Library(models.Model):
+    book_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=500)
+    authors = models.CharField(max_length=500)
+    average_rating = models.FloatField()
+    isbn = models.CharField(max_length=20, unique=True)
+    isbn13 = models.CharField(max_length=30, unique=True)
+    language_code = models.CharField(max_length=10)
+    num_pages = models.IntegerField()
+    ratings_count = models.IntegerField()
+    text_reviews_count = models.IntegerField()
+    publication_date = models.DateField()
+    publisher = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title"
+        
+        """
--- BookRecommendationSystem/myapp/views.py
+++ BookRecommendationSystem/myapp/views.py
@@ -1,5 +1,20 @@
+import json
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
+from .models import Library
+
+
 def autocomplete(request):
-    return render(request,"home.html") 
+    """     when LIbrary is name of the database
+    if 'term' in request.GET:
+        qs = Library.objects.filter(title__icontains=request.GET.get('term'))
+        titles = list()
+        for product in qs:
+            titles.append(product.title)
+
+        # titles = [product.title for product in qs]
+        return JsonResponse(titles, safe=False)
+        """
+    return render(request,"home.html")
