6df22262b42a2df94dbb1dabf76998ac58a3a1f3
pastelnata <ritabb04.05@gmail.com>
2025-03-12 14:04:53 +0100

    top10 page design

--- BookRecommendationSystem/myapp/urls.py
+++ BookRecommendationSystem/myapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path("",views.autocomplete,name="autocomplete")
+    path("",views.autocomplete,name="autocomplete"),
+    path("top10/", views.top10_page, name="top10_page")
 ]
