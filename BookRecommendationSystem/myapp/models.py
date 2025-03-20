38884d4390457f4af90ad54f98072be168c4afe1
filipsima <144452022+filipsima@users.noreply.github.com>
2025-03-20 11:18:01 +0100

    Fixed database issues

--- BookRecommendationSystem/myapp/models.py
+++ BookRecommendationSystem/myapp/models.py
@@ -4,14 +4,14 @@ class Book(models.Model):
     asin = models.CharField(max_length=20, unique=True, null=True, blank=True)
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    soldBy = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    soldBy = models.CharField(max_length=255, null=True, blank=True)
     productURL = models.CharField(max_length=255, unique=True, null=True, blank=True)
-    imgURL = models.CharField(max_length=255, unique=True, null=True, blank=True)
-    stars = models.IntegerField(null=True, blank=True, unique=True)
-    reviews = models.IntegerField(null=True, blank=True, unique=True)
-    price = models.IntegerField(null=True, blank=True, unique=True)
+    imgURL = models.CharField(max_length=255, null=True, blank=True)
+    stars = models.IntegerField(null=True, blank=True)
+    reviews = models.IntegerField(null=True, blank=True)
+    price = models.IntegerField(null=True, blank=True)
     isKindleUnlimited = models.BooleanField(default=False)
-    category_id = models.IntegerField(null=True, blank=True, unique=True)
+    category_id = models.IntegerField(null=True, blank=True)
     isBestSeller = models.BooleanField(default=False)
     isEditorsPick = models.BooleanField(default=False)
     isGoodReadsChoice = models.BooleanField(default=False)
@@ -20,30 +20,4 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
-
-#DON'T FORGET TO CHANGE FIELDS
-#ALSO MAKE MIGRATIONS 
-#python3 manage.py makemigrations myapp
-#python3 manage.py migrate
-
-# Create your models here.
-
-""" when Library is name of the database
-class Library(models.Model):
-    book_id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=500)
-    authors = models.CharField(max_length=500)
-    average_rating = models.FloatField()
-    isbn = models.CharField(max_length=20, unique=True)
-    isbn13 = models.CharField(max_length=30, unique=True)
-    language_code = models.CharField(max_length=10)
-    num_pages = models.IntegerField()
-    ratings_count = models.IntegerField()
-    text_reviews_count = models.IntegerField()
-    publication_date = models.DateField()
-    publisher = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.title"
         
-        """
