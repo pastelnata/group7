e8f54a886cf08e7a570a9c1162360770aada40b2
Adriana <adrianajaworska1717@gmail.com>
2025-03-23 11:52:06 +0100

    view

--- BookRecommendationSystem/myapp/views.py
+++ BookRecommendationSystem/myapp/views.py
@@ -1,24 +1,20 @@
 import json
 from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
-
+from .models import Book
 # Create your views here.
-#from .models import Library
 
+def libraryview(request):
+    event_list=Book.objects.all()
+    return render(request,"libraryview.html", {'event_list': event_list })
 
 def autocomplete(request):
-    """     when LIbrary is name of the database
-
-task1. create database
-task2 implement autosuggestions not only for title but also for author
-
     if 'term' in request.GET:
-        qs = Library.objects.filter(title__icontains=request.GET.get('term'))
+        qs = Book.objects.filter(title__icontains=request.GET.get('term'))
         titles = list()
         for product in qs:
             titles.append(product.title)
 
         # titles = [product.title for product in qs]
         return JsonResponse(titles, safe=False)
-        """
     return render(request,"home.html")
