250ca053023a6591056be81d2a195c802f4f3d5b
filipsima <144452022+filipsima@users.noreply.github.com>
2025-03-29 14:02:13 +0100

    Implemented BST for book titles

---
+++ BookRecommendationSystem/bst_search/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from . import views
+from django.http import JsonResponse
+
+def search_view(request):
+    query = request.GET.get('q', '')
+    limit = int(request.GET.get('limit', 25))
+    results = views.search_books(query, limit)
+    return JsonResponse({
+        'results': [
+            {
+                'title': book.title,
+                'author': book.author,
+                'stars': book.stars,
+                'reviews': book.reviews,
+                'price': book.price,
+            } for book in results
+        ]
+    })
+
+urlpatterns = [
+    path('search/', search_view, name='bst-search'),
+] 
