1f3ea9be8e47f561085d23d4a3a4ab172993b2ca
pastelnata <ritabb04.05@gmail.com>
2025-04-02 12:45:59 +0200

    Improve structure

--- BookRecommendationSystem/BookRecommendationSystem/settings.py
+++ BookRecommendationSystem/BookRecommendationSystem/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp', 
-    'bst_search'
+    'search'
 ]
 
 MIDDLEWARE = [
--- BookRecommendationSystem/search/apps.py
+++ BookRecommendationSystem/search/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BstSearchConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bst_search'
+    name = 'search'
