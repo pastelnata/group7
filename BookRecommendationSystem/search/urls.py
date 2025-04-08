081a72b6421115d1426e453e24a409dd3d22f271
pastelnata <ritabb04.05@gmail.com>
2025-04-08 15:45:11 +0200

    Remove dead code

--- BookRecommendationSystem/search/urls.py
+++ BookRecommendationSystem/search/urls.py
@@ -2,25 +2,6 @@ from django.urls import path
 from . import views
 from django.http import JsonResponse
 
-#def search_view(request):
-#    query = request.GET.get('q', '')
-#    limit = int(request.GET.get('limit', 25))
-#    results = views.search_books(query, limit)
-#    return JsonResponse({
-#        'results': [
-#            {
-#                'title': book.title,
-#                'author': book.author,
-#                'stars': book.stars,
-#                'reviews': book.reviews,
-#                'price': book.price,
-#            } for book in results
-#        ]
-#    })
-
-#def search_bar(request):
-#    return
-
 urlpatterns = [
     path('', views.search_results, name='bst-search'),
 ] 
