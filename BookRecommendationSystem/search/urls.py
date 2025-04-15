664c29984f9371b3e639376cdd6e482885f1704e
pastelnata <ritabb04.05@gmail.com>
2025-04-15 12:22:05 +0200

    feat: connect top10 page to heap sort algorithm

--- BookRecommendationSystem/search/urls.py
+++ BookRecommendationSystem/search/urls.py
@@ -3,5 +3,6 @@ from . import views
 from django.http import JsonResponse
 
 urlpatterns = [
-    path('', views.search_results, name='bst-search'),
+    path('', views.search_results, name='search_results'),
+    path('top10/', views.top10_results, name='top10_page')
 ] 
