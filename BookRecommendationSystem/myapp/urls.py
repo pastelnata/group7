19e2d19cb066cc344ec65417e994a1604216f766
pastelnata <ritabb04.05@gmail.com>
2025-04-15 12:22:05 +0200

    feat: connect top10 page to heap sort algorithm

--- BookRecommendationSystem/myapp/urls.py
+++ BookRecommendationSystem/myapp/urls.py
@@ -1,14 +1,12 @@
-from django.urls import path
+from django.urls import include, path
 from . import views 
-from search.views import search_results
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('homepage/', views.homepage, name='homepage'), 
     path('autocomplete/',views.autocomplete,name="autocomplete"),
-    path("top10/", views.top10_page, name="top10_page"),
     path('library/',views.libraryview,name="libraryview"),
     path('support/', views.support_view, name='support'),
-    path('search/', search_results, name='search_results')
+    path('', include('search.urls'))
 ]
 
--- BookRecommendationSystem/search/sorting/get_top10.py
+++ BookRecommendationSystem/search/sorting/get_top10.py
@@ -1,11 +1,8 @@
 from books.models import Book
 from .heap_sort import HeapSort
 
-
-books = Book.objects.only('stars')
-
 def get_books_by_categories(category):
-    return Book.objects.filter(category_name=category).only('stars', 'title')
+    return Book.objects.filter(category_name=category).only('stars', 'title', 'author', 'price', 'imgURL', 'productURL')
 
 def get_top10_books():
     categories = ['Romance', 'Mystery, Thriller & Suspense', 'History', 'Health, Fitness & Dieting', 'Science Fiction & Fantasy', 'Education & Teaching']
@@ -13,8 +10,31 @@ def get_top10_books():
 
     for category in categories:
         books = get_books_by_categories(category)
-        books_with_stars = [(book.stars, book.title) for book in books] 
-        top_books_for_category = HeapSort.topK(books_with_stars, 10) 
-        top_books[category] = [title for _, title in top_books_for_category]
+
+        top10s = sort_by_stars(books)
+
+        books_dict = get_book_info(top10s)
+
+        top_books[category] = [
+            {
+                'title': books_dict[book_id].title,
+                'stars': stars,
+                'author': books_dict[book_id].author,
+                'price': books_dict[book_id].price,
+                'imgURL': books_dict[book_id].imgURL,
+                'productURL': books_dict[book_id].productURL,
+            }
+            for stars, book_id in top10s
+        ]
 
     return top_books
+
+def sort_by_stars(books):
+    books_with_stars = [(book.stars, book.id) for book in books]
+    top_books_for_category = HeapSort.topK(books_with_stars, 10)
+    return top_books_for_category
+
+def get_book_info(books):
+    book_ids = [book_id for _, book_id in books]
+    books_dict = {book.id: book for book in Book.objects.filter(id__in=book_ids)}
+    return books_dict
