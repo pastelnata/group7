0ed816c399e5e056baffbe744e66f63c4f7e93ac
Adriana <adrianajaworska1717@gmail.com>
2025-05-03 20:04:24 +0200

    polishing design

--- BookRecommendationSystem/myapp/urls.py
+++ BookRecommendationSystem/myapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('homepage/', views.homepage, name='homepage'), 
     path('autocomplete/',views.autocomplete,name="autocomplete"),
     path('library/', views.libraryview, name='library'),
+    
     path('support/', views.support_view, name='support'),
     path('', include('search.urls'))
 ]
