6a4717209a4025f1e76a38618011241d9f2b6e0e
filipsima <fisim23@student.sdu.dk>
2025-05-16 13:07:49 +0200

    Added graph-based recommendation system (first version)

--- BookRecommendationSystem/books/models.py
+++ BookRecommendationSystem/books/models.py
@@ -3,21 +3,76 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     asin = models.CharField(max_length=20, unique=True, null=True, blank=True)
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, db_index=True)
+    author = models.CharField(max_length=255, db_index=True)
     soldBy = models.CharField(max_length=255, null=True, blank=True)
-    productURL = models.CharField(max_length=255, unique=True, null=True, blank=True)
-    imgURL = models.CharField(max_length=255, null=True, blank=True)
-    stars = models.IntegerField(null=True, blank=True)
-    reviews = models.IntegerField(null=True, blank=True)
-    price = models.IntegerField(null=True, blank=True)
-    isKindleUnlimited = models.BooleanField(default=False)
-    category_id = models.IntegerField(null=True, blank=True)
-    isBestSeller = models.BooleanField(default=False)
-    isEditorsPick = models.BooleanField(default=False)
-    isGoodReadsChoice = models.BooleanField(default=False)
+    productURL = models.URLField(max_length=500, null=True, blank=True)
+    imgURL = models.URLField(max_length=500, null=True, blank=True)
+    stars = models.FloatField(null=True, blank=True, db_index=True)
+    reviews = models.IntegerField(null=True, blank=True, db_index=True)
+    price = models.FloatField(null=True, blank=True, db_index=True)
+    isKindleUnlimited = models.BooleanField(default=False, db_index=True)
+    category_id = models.IntegerField(null=True, blank=True, db_index=True)
+    isBestSeller = models.BooleanField(default=False, db_index=True)
+    isEditorsPick = models.BooleanField(default=False, db_index=True)
+    isGoodReadsChoice = models.BooleanField(default=False, db_index=True)
     published_date = models.DateField(null=True, blank=True)
-    category_name = models.CharField(max_length=10, null=True, blank=True)
+    category_name = models.CharField(max_length=255, null=True, blank=True, db_index=True)
 
     def __str__(self):
         return self.title
+
+    @classmethod
+    def get_unique_categories(cls):
+        """Get all unique categories from the database"""
+        return cls.objects.exclude(category_name__isnull=True).values_list('category_name', flat=True).distinct()
+
+    @classmethod
+    def get_popular_books(cls, limit=10):
+        """Get popular books based on ratings and reviews"""
+        return cls.objects.exclude(stars__isnull=True).order_by('-stars', '-reviews')[:limit]
+
+    @classmethod
+    def get_best_sellers(cls):
+        """Get all bestseller books"""
+        return cls.objects.filter(isBestSeller=True)
+
+    @classmethod
+    def get_editors_picks(cls):
+        """Get all editor's pick books"""
+        return cls.objects.filter(isEditorsPick=True)
+
+    @classmethod
+    def get_goodreads_choices(cls):
+        """Get all Goodreads Choice winners"""
+        return cls.objects.filter(isGoodReadsChoice=True)
+
+    def get_similar_books(self, limit=5):
+        """Get similar books based on category and author"""
+        similar_books = set()
+        
+        category_matches = Book.objects.filter(
+            category_name=self.category_name
+        ).exclude(id=self.id)
+        
+        author_matches = Book.objects.filter(
+            author=self.author
+        ).exclude(id=self.id)
+        
+        for book in category_matches:
+            similar_books.add((book, 1))
+        
+        for book in author_matches:
+            similar_books.add((book, 2))
+        
+        return sorted(similar_books, key=lambda x: x[1], reverse=True)[:limit]
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['category_name', 'stars']),
+            models.Index(fields=['price', 'stars']),
+            models.Index(fields=['isBestSeller', 'stars']),
+            models.Index(fields=['isEditorsPick', 'stars']),
+            models.Index(fields=['isGoodReadsChoice', 'stars']),
+            models.Index(fields=['isKindleUnlimited', 'stars']),
+        ]
--- BookRecommendationSystem/search/search/graph.py
+++ BookRecommendationSystem/search/search/graph.py
@@ -2,53 +2,58 @@ import networkx as nx
 from collections import defaultdict
 from books.models import Book
 from collections import deque, defaultdict
-from books.models import Book
 
 def build_graph_adj_list():
     """
-    Builds an adjacency list where books are connected if they share the same category.
+    Builds an optimized adjacency list where books are connected if they share the same category.
     """
     books = list(Book.objects.all())
+    
     adj = defaultdict(list)
     category_groups = defaultdict(list)
+    book_lookup = {}
 
-    # Group books by category
     for book in books:
-        category_groups[book.category_id].append(book)
-        if book.id not in adj:
-            adj[book.id] = []
-
-    # Create edges between all books in the same category
+        if book.category_id:  
+            category_groups[book.category_id].append(book)
+            book_lookup[book.id] = book
+            adj[book.id] = []  
     for group in category_groups.values():
-        for i in range(len(group)):
-            for j in range(i + 1, len(group)):
-                b1, b2 = group[i], group[j]
-                adj[b1.id].append(b2.id)
-                adj[b2.id].append(b1.id)
+        if len(group) > 1:
+            for i in range(len(group)):
+                for j in range(i + 1, len(group)):
+                    b1, b2 = group[i], group[j]
+                    adj[b1.id].append(b2.id)
+                    adj[b2.id].append(b1.id)
 
-    return adj, {book.id: book for book in books}  # Return ID-to-object mapping too
-
+    return adj, book_lookup
 
 def build_graph():
+    """
+    Builds an optimized NetworkX graph where books are connected if they share the same category.
+    """
     G = nx.Graph()
 
-    # Get all books
     books = list(Book.objects.all())
 
-    # Add nodes
     for book in books:
-        G.add_node(book.id, title=book.title, category=book.category_id)
+        if book.category_id: 
+            G.add_node(book.id, 
+                      title=book.title, 
+                      category=book.category_id,
+                      stars=book.stars or 0,
+                      reviews=book.reviews or 0)
 
-    # Group books by category
     category_groups = defaultdict(list)
     for book in books:
-        category_groups[book.category_id].append(book)
+        if book.category_id: 
+            category_groups[book.category_id].append(book)
 
-    # Add edges within same category
     for group in category_groups.values():
-        for i in range(len(group)):
-            for j in range(i + 1, len(group)):
-                G.add_edge(group[i].id, group[j].id, weight=1)
+        if len(group) > 1:  
+            for i in range(len(group)):
+                for j in range(i + 1, len(group)):
+                    G.add_edge(group[i].id, group[j].id, weight=1)
 
     return G
 
