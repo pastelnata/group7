aa4b6083dd8137a37bd4f2ce08a94269911f0a07
pastelnata <ritabb04.05@gmail.com>
2025-05-27 14:33:50 +0200

    fix: navbar current active page, different search page navbar

--- BookRecommendationSystem/myapp/urls.py
+++ BookRecommendationSystem/myapp/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
+    path('search/', include('search.urls')),
+    path('recommendation/', include('recommendation.urls')),
     path('autocomplete/',views.autocomplete,name="autocomplete"),
     path('about/', views.about, name='about'), 
     path('library/', views.libraryview, name='libraryview'),
     path('support/', views.support_view, name='support'),
-    path('', include('search.urls')),
-    path('', include('recommendation.urls')),
 ]
 
